6ef0f05ad2fb240edca3d2492fbafa4c8f38afd6
Alfredolsl <alfredo.445566@gmail.com>
2024-01-24 12:46:33 -0800

    prints the number of and the list of its args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,11 +3,13 @@ import sys
 num_args = sys.argv
 args_len = len(num_args) - 1
 
-print("{} arguments".format(args_len), end="")
-if args_len > 0:
-    print(":")
+print("{}".format(args_len), end=" ")
+if args_len == 0:
+    print("arguments:")
+elif args_len == 1:
+    print("argument:")
 else:
-    print(".")
+    print("arguments:")
 
 for i in range(args_len):
     print("{}: {}".format(i + 1, num_args[i + 1]))
