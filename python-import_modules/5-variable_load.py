264ab2af3173ca2e8fc95cf4e51313e3e98e09c4
Alfredolsl <alfredo.445566@gmail.com>
2024-01-24 14:11:56 -0800

    imports the variable a from the file variable_load_5.py

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -3,5 +3,5 @@
 if __name__ == "__main__":
     """imports the variable a from the file 
     variable_load_5.py and prints its value"""
-    import variable_load_5
-    print("{}".format(variable_load_5.a))
+    from variable_load_5 import a
+    print(a)
