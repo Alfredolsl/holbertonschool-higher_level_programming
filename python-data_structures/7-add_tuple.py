aa68dcea1fd9b5f0d865e24510fd1044540fd49f
Alfredolsl <alfredo.445566@gmail.com>
2024-01-25 10:48:10 -0800

    adds 2 tuples

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    """adds 2 tuples"""
+    tuple1 = tuple_a + (0, 0) 
+    tuple2 = tuple_b + (0, 0)
+
+    combined_tuples = tuple1[0] + tuple2[0], tuple1[1] + tuple2[1]
+    return combined_tuples
