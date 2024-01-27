7680d8ab3df36a232ef6686f4e68aa5a1a0bf4cd
Alfredolsl <alfredo.445566@gmail.com>
2024-01-27 08:09:59 -0800

    pycodestyle compliant

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    """replaces an element in a list at a specific pos without modifying the original list"""
+    """replaces an element in a list at a specific
+    pos without modifying the original list"""
     if idx < 0 or idx > len(my_list) - 1:
         return my_list.copy()
 
--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    """returns a tuple with the length of a string and its character""" 
+    """returns a tuple with the length of a string and its character"""
     len_char_tuple = (0, 'None')
     if sentence:
         len_char_tuple = (len(sentence), sentence[0])
