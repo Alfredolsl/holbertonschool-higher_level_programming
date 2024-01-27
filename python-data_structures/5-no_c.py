263256d7d17a84ca4ae2bb18dc46b834e4b8bc0c
Alfredolsl <alfredo.445566@gmail.com>
2024-01-27 08:12:29 -0800

    removed parentheses after return

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,4 +2,4 @@
 def no_c(my_string):
     """"removes all characters c and C from string"""
     new_string = my_string.translate({ord(i): None for i in 'Cc'})
-    return(new_string)
+    return new_string
