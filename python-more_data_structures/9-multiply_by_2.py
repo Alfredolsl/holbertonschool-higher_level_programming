05ecfb5002ef7be69f4174e120bec0defe7e6d27
Alfredolsl <alfredo.445566@gmail.com>
2024-01-31 15:41:27 -0800

    pycodestyle

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    dict_mul = {key:value * 2 for key, value in a_dictionary.items()}
+    dict_mul = {key: value * 2 for key, value in a_dictionary.items()}
     return dict_mul
