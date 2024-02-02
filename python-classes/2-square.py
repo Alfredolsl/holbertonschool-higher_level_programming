e675c0f58ee6050a96285850ef4d539d2d0c0e94
Alfredolsl <alfredo.445566@gmail.com>
2024-02-02 14:22:57 -0800

    size validatiom

---
+++ python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""defines a square with size as integer with setter"""
+
+
+class Square:
+    """represents a square"""
+
+
+    def __init__(self, size=0):
+        """Initializes a new Square.
+
+        Args:
+            size (int): The size of the new square
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
