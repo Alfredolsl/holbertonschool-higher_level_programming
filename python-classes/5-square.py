75e0942603a74a4988229e96c5f5f5fa91f2fb57
Alfredolsl <alfredo.445566@gmail.com>
2024-02-03 09:59:15 -0800

    Printing a square

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -23,9 +23,9 @@ class Square:
     def size(self, value):
         """sets the proper value for size"""
         if not isinstance(value, int):
-            return TypeError("size must be an integer")
+            raise TypeError("size must be an integer")
         elif value < 0:
-            return ValueError("size must be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
