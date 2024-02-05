ae4648267b49e24ec081184706240f685088b0d5
Alfredolsl <alfredo.445566@gmail.com>
2024-02-04 19:07:29 -0800

    pycodestyle compliant

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -18,14 +18,16 @@ def matrix_divided(matrix, div):
     Returns:
         A matrix with the results of the division.
     """
-    if (matrix == [] or
-    not isinstance(matrix, list) or
-    not all(isinstance(row, list) for row in matrix)):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if (matrix == [] or not isinstance(matrix, list)
+            or not all(isinstance(row, list) for row in matrix)):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
 
-    if not all(isinstance(elem, (int, float)) for row in matrix for elem in row):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
+    if not (all(isinstance(elem, (int, float))
+            for row in matrix for elem in row)):
+        raise TypeError("matrix must be a matrix (list of lists) "
+                        "of integers/floats")
+
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
