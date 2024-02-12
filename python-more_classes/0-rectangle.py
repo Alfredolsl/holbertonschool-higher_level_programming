0c14396e5e5ea2728f14b8b17ec2ffc854435aa2
root <alfredo.445566@gmail.com>
2024-02-12 19:55:51 +0000

    Simple rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""defines a class rectangle"""
+
+class Rectangle:
+    """represents a rectangle"""
+
+    def __init__(self):
+        """initializes a rectangle"""
+        pass
