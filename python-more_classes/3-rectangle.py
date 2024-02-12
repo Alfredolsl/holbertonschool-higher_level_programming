7bb9a09d55b226402b58e9acdff06532f4dd0be8
root <alfredo.445566@gmail.com>
2024-02-12 20:37:02 +0000

    String representation

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -56,7 +56,7 @@ class Rectangle:
     def __str__(self):
         """Returns the size of the rectangle with '#' characters"""
         if self.__height == 0 or self.__width == 0:
-            return 0
+            return ""
 
         rect = []
         for n in range(self.__height):
