bdf52a135318fa9333de091a713dd2091226f8c3
root <alfredo.445566@gmail.com>
2024-02-13 00:38:37 +0000

    Compare rectangles

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -86,10 +86,10 @@ class Rectangle:
             rect_1 (int): Rectangle 1
             rect_2 (int): Rectangle 2
         """
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be a rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be a rectangle")
+        if rect_1.__class__.__name__ != "Rectangle":
+            raise TypeError("rect_1 must be a Rectangle")
+        if rect_2.__class__.__name__ != "Rectangle":
+            raise TypeError("rect_2 must be a Rectangle")
 
         if rect_1.area() >= rect_2.area():
             return rect_1
