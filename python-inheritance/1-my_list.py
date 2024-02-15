da5003bef8a763f3b21455f550b849d3a18bac71
root <alfredo.445566@gmail.com>
2024-02-15 02:12:38 +0000

    My list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a MyList class"""
+
+
+class MyList(list):
+    """A subclass of list."""
+    def __init__(self):
+        """Initializes MyList."""
+        super().__init__()
+
+    def print_sorted(self):
+        """Prints a sorted list."""
+        print(sorted(self))
