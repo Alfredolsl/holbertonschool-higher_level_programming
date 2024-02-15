ac4c0430d2b403520a8737508502c7b528521067
root <alfredo.445566@gmail.com>
2024-02-15 17:40:40 +0000

    Rectangle

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """Represents a rectangle."""
+    def __init__(self, width, height):
+        """Initializes Rectangle.
+
+        Args:
+            width (int): Width of rectangle.
+            height (int): Height of rectangle.
+        """
+        super().__init__
+        self.__width = self.integer_validator("width", width)
+        self.__height = self.integer_validator("height", height)
