8d5fc82b7fa871f57dcaa15f58f9b01b60472b52
root <alfredo.445566@gmail.com>
2024-02-16 03:18:26 +0000

    Pascal

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines a function."""
+
+
+def pascal_triangle(n):
+    """Returns lists of integers representing
+    the Pascal's triangle of n."""
+
+    if n <= 0:
+        return []
+
+    result = []
+    last_list = []
+
+    for i in range(1, n + 1):
+        temp_list = []
+
+        for j in range(0, i):
+            if j == 0 or j == i - 1:
+                temp_list.append(1)
+            else:
+                sum_two_nums = last_list[j] + last_list[j - 1]
+                temp_list.append(sum_two_nums)
+
+        result.append(temp_list)
+        last_list = temp_list
+
+    return result
