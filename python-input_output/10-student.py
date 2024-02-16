4ef06c21297e314a2c63746b5e58479aeb3d8432
root <alfredo.445566@gmail.com>
2024-02-16 01:46:32 +0000

    Student to JSON with filter

---
+++ python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines a class Student."""
+
+
+class Student():
+    """Represents a student."""
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes Student instance.
+
+        Args:
+            first_name (str): First name of student.
+            last_name (str): Last name of student.
+            age (str): Age of student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list and all([type(i) is str for i in attrs]):
+            return {key: getattr(self, key)
+                    for key in attrs if hasattr(self, key)}
+        return self.__dict__
