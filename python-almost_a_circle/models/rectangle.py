0d95d69cf3831c35ce073c0a8990296836621c51
root <alfredo.445566@gmail.com>
2024-02-20 17:48:59 +0000

    Added area function, display rectangle on screen function, and override __str__ method to return info of rectangle attributes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -35,8 +35,8 @@ class Rectangle(Base):
     def width(self, value):
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -47,8 +47,8 @@ class Rectangle(Base):
     def height(self, value):
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -74,3 +74,16 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def display(self):
+        for i in range(self.__height):
+            print("#" * self.__width)
+
+    def __str__(self):
+        return "[{}] ({}) {}/{} - {}/{}".format(self.__class__.__name__,
+                                                self.id,
+                                                self.__x, self.__y,
+                                                self.__width, self.__height)
