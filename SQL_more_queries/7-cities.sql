4ef1aac0427869c58004b682d2b59677225147ff
root <alfredo.445566@gmail.com>
2024-03-18 21:45:48 +0000

    removed description

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,9 +1,6 @@
 -- creates the db hbtn_0d_usa and the table cities in said db.
--- create database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
--- use database
 USE htbn_0d_usa;
--- create table cities
 CREATE TABLE IF NOT EXISTS cities (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL,
 	state_id INT NOT NULL,
