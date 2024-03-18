4581df6bce3828183d5b60bf359cd81bbf88383c
root <alfredo.445566@gmail.com>
2024-03-18 05:51:06 +0000

    creates db hbtn_0d_usa and the table cities from said db on your MySQL server

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -7,7 +7,7 @@ USE htbn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL,
 	state_id INT NOT NULL,
-	name VARCHAR(256),
+	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id),
 	FOREIGN KEY(state_id) REFERENCES states(id)
 );
