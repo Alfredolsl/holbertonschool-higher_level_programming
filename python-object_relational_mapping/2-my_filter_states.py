db966003b5b63a7857f9c46dabe5ba5f8db609bd
root <alfredo.445566@gmail.com>
2024-03-23 21:49:49 +0000

    takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Displays all values in states of hbnt_0e_0_usa where name matches arg """
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port= 3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cur = db.cursor()
+    arg = argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'".format(arg))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
