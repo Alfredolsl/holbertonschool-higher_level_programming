7b8971b3cffa2975d67a7321fd5950df2bce750f
root <alfredo.445566@gmail.com>
2024-03-23 22:14:42 +0000

    same as 2-my_filter_states.py but with SQL injection protection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""takes args and shows matching values in states safe from MySQL injections"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cur = db.cursor()
+    arg = argv[4]
+
+    # We use a tuple, which is immutable (cannot be modified after creation)
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s", (arg, ))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    
+    cur.close()
+    db.close()
