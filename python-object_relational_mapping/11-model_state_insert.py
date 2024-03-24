378078b9ae158757be40f87f5a87509585c34efd
root <alfredo.445566@gmail.com>
2024-03-24 05:20:46 +0000

    adds the State object “Louisiana” to the database hbtn_0e_6_usa

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""adds the State object "Louisian" to the db hbtn_0e_6_usa"""
+from sys import argv
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    URL = "mysql+mysqldb://{}:{}\
+           @localhost:3306/{}".format(argv[1], argv[2], argv[3])
+    engine = create_engine(URL)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    newState = State(name="Louisiana")
+    session.add(newState)
+
+    instance = session.query(State).\
+        filter_by(name="Louisiana").first()
+
+    print(instance.id)
+    session.commit()
