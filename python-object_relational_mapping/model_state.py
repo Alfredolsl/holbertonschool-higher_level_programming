2c7e50dbfd1bc212322a115f840b2b5216950150
root <alfredo.445566@gmail.com>
2024-03-24 00:07:04 +0000

    fixed string value without quotes

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -11,6 +11,6 @@ class State(Base):
     """
     Class with id and name attributes for each state
     """
-    __tablename__ = states
+    __tablename__ = "states"
     id = Column(Integer, primary_key=True, unique=True, nullable=False)
     name = Column(String(128), nullable=False)
