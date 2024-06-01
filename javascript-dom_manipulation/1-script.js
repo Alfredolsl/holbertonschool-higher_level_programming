cd9ff8a0685606fd7a76eaa321f5a2c589c85d00
root <alfredo.445566@gmail.com>
2024-06-01 05:48:49 +0000

    switch to camelcase

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,9 +1,9 @@
 // updates text color of header element to red when user
 // clicks on the tag with id 'red_header'
-// tag_red_header = document.getElementById('red_header');
-const red_header = document.getElementById('red_header');
+// tag_redHeader = document.getElementById('red_header');
+const redHeader = document.getElementById('red_header');
 const header = document.querySelector('header');
 
-red_header.onclick = function() {
+redHeader.onclick = function() {
   header.style.color = '#FF0000';
 };
