0bfc7f197d6a05a52aca3145d707fda00670c6c4
root <alfredo.445566@gmail.com>
2024-06-01 04:54:58 +0000

    add task 1

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,8 @@
+// updates text color of header element to red when user
+// clicks on the tag with id 'red_header'
+tag_red_header = document.getElementById('red_header');
+const header = document.querySelector('header');
+
+object.onclick = function() {
+  header.style.color = '#FF0000';
+};
